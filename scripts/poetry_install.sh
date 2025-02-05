b140b77c12749658070fea9d943e1fcb592a7c87
Aymen MEJRI <BL6593@engie.com>
2025-02-05 10:47:38 +0100

    UPDATE poetry install script

--- scripts/poetry_install.sh
+++ scripts/poetry_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# install pipx
+# Install pipx
 sudo apt install pipx
 
-# install poetry via pipx
+# Install poetry via pipx
 pipx install poetry
 
 # Ensure that the poetry path is added to ~/.bashrc file
@@ -12,17 +12,3 @@ pipx ensurepath
 pip install poetry-plugin-shell
 
 # Your need to restart terminal for the changes to take effect.
-
-# Configure Poetry to Use a .venv in the Current Directory
-poetry config virtualenvs.in-project true
-
-# Init poetry project by creating pyproject.toml file
-poetry init
-
-# Create the Virtual Environment
-poetry install
-
-# Activate the Virtual Environment
-poetry shell
-
-
